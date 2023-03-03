6621902223d5b275b1bae55064005cccc58088a0
1stApplePie <105532497+1stApplePie@users.noreply.github.com>
2023-03-03 22:08:35 +0900

    Update upload.py

--- _posts/upload.py
+++ _posts/upload.py
@@ -7,7 +7,7 @@ day = d.strftime("{}-{}-{}-".format(d.strftime("%y"), d.strftime("%m"), d.strfti
 
 # Rename README.md
 before_md_name = ''
-md_path = 'C:/Users/Administrator/Desktop/github/Programmers_cpp-main/'
+md_path = 'C:/Users/Administrator/Desktop/github/Programmers-main/'
 file_names = os.listdir(md_path)
 for name in file_names:
     if name[-3:] == '.md':
@@ -19,14 +19,14 @@ dst = os.path.join(md_path, day + "README.md")
 os.rename(src, dst)
 
 # File Rename & Edit README.md
-dir_list = ['lv1',  'unrated']
+dir_list = ['lv1', 'lv2', 'lv3',  'unrated']
 file_path = md_path + '프로그래머스/'
 md_path = md_path + day + "README.md"
 head_py = "---\nlayout: single\ntitle: Programmers_py\ncategories: Programmers_py\n---\n"
 head_cpp = "---\nlayout: single\ntitle: Programmers_cpp\ncategories: Programmers_cpp\n---\n"
-main = "\nC++ 관련 코딩테스트 문제풀이 기록소입니다.\n"
+main = "\nPython 관련 코딩테스트 문제풀이 기록소입니다.\n"
 with open(md_path, 'w', encoding = "UTF-8") as f:
-    f.write(head_cpp)
+    f.write(head_py)
     f.write(main)
     f.close()
         
@@ -53,7 +53,7 @@ for dir in dir_list:
         n_name = re.sub('\s', '-', name)
         print(n_name)
         with open(md_path, 'a', encoding = "UTF-8") as f:
-            f.write('* [{}](https://1stapplepie.github.io/programmers_cpp/{}/)\n'.format(name, n_name))
+            f.write('* [{}](https://1stapplepie.github.io/programmers_py/{}/)\n'.format(name, n_name))
             f.close()
 
 ## Edit Solution
@@ -75,7 +75,17 @@ for dir in dir_list:
                     contain_cpp = f.read()
                     f.close()
         for sol in Solution:
-            if sol[-3:] == ".md" and contain_cpp != "":
+            if sol[-3:] == ".md" and contain_py != "":
+                with open(file_path_in + "/" + name + "/" + sol, 'r', encoding = "UTF-8") as f:
+                    temp = f.read()
+                with open(file_path_in + "/" + name + "/" + sol, 'w', encoding = "UTF-8") as f:
+                    f.write(head_py)
+                    f.write(temp)
+                    f.write("\n\n```py\n")
+                    f.write(contain_py)
+                    f.write("\n\n```\n")
+                    f.close()   
+            elif sol[-3:] == ".md" and contain_cpp != "":
                 with open(file_path_in + "/" + name + "/" + sol, 'r', encoding = "UTF-8") as f:
                     temp = f.read()
                 with open(file_path_in + "/" + name + "/" + sol, 'w', encoding = "UTF-8") as f:
@@ -85,16 +95,7 @@ for dir in dir_list:
                     f.write(contain_cpp)
                     f.write("\n\n```\n")
                     f.close()
-            elif sol[-3:] == ".md" and contain_py != "":
-                with open(file_path_in + "/" + name + "/" + sol, 'r', encoding = "UTF-8") as f:
-                    temp = f.read()
-                with open(file_path_in + "/" + name + "/" + sol, 'w', encoding = "UTF-8") as f:
-                    f.write(head_py)
-                    f.write(temp)
-                    f.write("\n```py\n")
-                    f.write(contain_py)
-                    f.write("\n\n```")
-                    f.close()        
+                 
 # Solution README file Rename
     for name in file_names_in:
         src = os.path.join(file_path_in + name + "/" , "README.md")
